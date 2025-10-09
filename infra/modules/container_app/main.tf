78d47466326dd6f85c5d8bdad30ab26c5edee35d
mani7reddy <111497953+mani7reddy@users.noreply.github.com>
2025-10-09 15:06:10 -0500

    Update main.tf

--- infra/modules/container_app/main.tf
+++ infra/modules/container_app/main.tf
@@ -1,1 +1,30 @@
+resource "azurerm_container_app_environment" "env" {
+  name                = "cae-dotnet-${var.environment}"
+  location            = var.location
+  resource_group_name = var.rg_name
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = "dotnethelloworld-${var.environment}"
+  resource_group_name          = var.rg_name
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  revision_mode                = "Single"
+  ingress {
+    external_enabled = true
+    target_port      = 80
+  }
+
+  template {
+    container {
+      name   = "dotnethelloworld"
+      image  = var.image_name
+      cpu    = 0.5
+      memory = "1Gi"
+    }
+  }
+}
+
+output "app_url" {
+  value = azurerm_container_app.app.latest_revision_fqdn
+}
 
