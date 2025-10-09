6d06f0b7de9c3b16534b5aee69c31ba533fd661f
mani7reddy <111497953+mani7reddy@users.noreply.github.com>
2025-10-09 15:05:48 -0500

    Update main.tf

--- infra/modules/resource_group/main.tf
+++ infra/modules/resource_group/main.tf
@@ -1,1 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-dotnet-${var.environment}"
+  location = var.location
+}
+
+output "name" {
+  value = azurerm_resource_group.rg.name
+}
+output "location" {
+  value = azurerm_resource_group.rg.location
+}
 
