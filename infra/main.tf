e885d8b9591769bbc3e657effd0a25abf9766343
mani7reddy <111497953+mani7reddy@users.noreply.github.com>
2025-10-09 15:02:46 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,31 @@
 
+terraform {
+  required_version = ">=1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.100"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+module "rg" {
+  source      = "./modules/resource_group"
+  environment = var.environment
+  location    = var.location
+}
+
+# Container App Environment + App
+module "containerapp" {
+  source       = "./modules/container_app"
+  rg_name      = module.rg.name
+  location     = module.rg.location
+  environment  = var.environment
+  image_name   = var.image_name
+}
