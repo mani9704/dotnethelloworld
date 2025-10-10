c5c6b72eed61fb6c1659582a57fb403faf2891e1
mani7reddy <111497953+mani7reddy@users.noreply.github.com>
2025-10-10 08:43:02 -0500

    Update main.tf

--- infra/modules/container_app/main.tf
+++ infra/modules/container_app/main.tf
@@ -1,24 +1,9 @@
-variable "rg_name" {}
-variable "location" {}
-variable "environment" {}
-variable "image_name" {}
-
-resource "azurerm_container_app_environment" "env" {
-  name                = "cae-dotnet-${var.environment}"
-  location            = var.location
-  resource_group_name = var.rg_name
-}
-
 resource "azurerm_container_app" "app" {
   name                         = "dotnethelloworld-${var.environment}"
-  resource_group_name          = var.rg_name
-  container_app_environment_id = azurerm_container_app_environment.env.id
-  revision_mode                = "Single"
-
-  ingress {
-    external_enabled = true
-    target_port      = 80
-  }
+  resource_group_name           = var.rg_name
+  location                      = var.location
+  container_app_environment_id  = azurerm_container_app_environment.env.id
+  revision_mode                 = "Single"
 
   template {
     container {
@@ -26,10 +11,31 @@ resource "azurerm_container_app" "app" {
       image  = var.image_name
       cpu    = 0.5
       memory = "1Gi"
+
+      env {
+        name  = "ASPNETCORE_ENVIRONMENT"
+        value = var.environment
+      }
     }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 80
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  tags = {
+    environment = var.environment
   }
 }
 
-output "app_url" {
-  value = "https://${azurerm_container_app.app.latest_revision_fqdn}"
+resource "azurerm_container_app_environment" "env" {
+  name                = "env-${var.environment}"
+  location            = var.location
+  resource_group_name = var.rg_name
 }
