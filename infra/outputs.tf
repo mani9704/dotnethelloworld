57af09bd19ae2eb3c871ed564b7d3dd902b626f3
mani7reddy <111497953+mani7reddy@users.noreply.github.com>
2025-10-10 12:53:25 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatestorage9704"  # must be globally unique
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+output "app_url" {
+  value = module.container_app.app_url
 }
