d3931923f9f5dc53c3c02d817ccc859984de78d4
mani7reddy <111497953+mani7reddy@users.noreply.github.com>
2025-10-10 08:16:42 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,16 +1,14 @@
 variable "environment" {
   type        = string
-  description = "Deployment environment name"
+  description = "Deployment environment"
 }
 
 variable "location" {
   type        = string
   description = "Azure region"
-  default     = "East US"
 }
 
 variable "image_name" {
   type        = string
-  description = "Docker image name (e.g. ghcr.io/mani/dotnethelloworld:latest)"
+  description = "Docker image name"
 }
-
