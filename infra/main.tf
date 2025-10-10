2928032f32932d42f9df89cb68148a8eae0e6b20
mani7reddy <111497953+mani7reddy@users.noreply.github.com>
2025-10-10 10:24:39 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">=1.5.0"
-  
-backend "azurerm" {
-    resource_group_name   = "rg-dotnet-dev"
-    storage_account_name  = "tfstatestoredev"      # must be globally unique
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
 required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
